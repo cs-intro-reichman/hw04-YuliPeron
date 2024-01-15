a8aab26c3857d541897a7934521863be935e6316
Yuli peron <yulilidya.peron@post.runi.ac.il>
2024-01-16 00:30:08 +0200

    string

--- StringOps.java
+++ StringOps.java
@@ -22,17 +22,81 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String str = "   Intro to coMPUter   sCIEncE";
+        System.out.println(camelCase(str));
     }
 
     public static String capVowelsLowRest (String string) {
         // Write your code here:
-        return "";
+        String newString = "";
+        for (int i =0; i<string.length(); i++)
+        {
+            if (string.charAt(i) >= 'A' && string.charAt(i)<= 'Z')
+            {
+                if((string.charAt(i)!= 'A')&&(string.charAt(i)!= 'E')&&(string.charAt(i)!= 'I')&&(string.charAt(i)!= 'O')&&(string.charAt(i)!= 'U')) {
+                    newString += (char) (string.charAt(i) + 32);
+                }
+                else {
+                    newString += string.charAt(i);
+                }
+            }
+            else{
+                if((string.charAt(i)== 'a')||(string.charAt(i)== 'e')||(string.charAt(i)== 'i')||(string.charAt(i)== 'o')||(string.charAt(i)== 'u')) {
+                    newString += (char) (string.charAt(i) - 32);
+                }
+                else {
+                    newString += string.charAt(i);
+                }
+            }
+        }
+        return newString;
+    }
+
+    public static String lowerCase(String s) {
+        // Replace the following statement with your code
+        String newString = "";
+        for (int i =0; i<s.length(); i++)
+        {
+            if (s.charAt(i) >= 'A' && s.charAt(i)<= 'Z')
+            {
+                newString+=(char)(s.charAt(i)+32);
+            }
+            else{
+                newString += s.charAt(i);
+            }
+
+        }
+        return newString;
     }
 
     public static String camelCase (String string) {
         // Write your code here:
-        return "";
+        String newString = "";
+        String temp = lowerCase(string);
+        int tempIndex = 0;
+        if(temp.charAt(0) != 32) {
+            newString += temp.charAt(0);
+        }
+        else {
+            while (temp.charAt(tempIndex) == 32) {
+                newString = newString;
+                tempIndex ++;
+            }
+            newString += temp.charAt(tempIndex);
+        }
+        for (int i =tempIndex+1 ; i<temp.length(); i++) {
+            if ((temp.charAt(i) == 32 ) && (temp.charAt(i-1) == 32 )) {
+                newString = newString;
+            }
+
+            else if ((temp.charAt(i-1) == 32) && (temp.charAt(i) != 32)) {
+                newString += (char)(temp.charAt(i) -32);
+            }
+            else if ((temp.charAt(i-1) != 32) && (temp.charAt(i) != 32) ) {
+                newString += (temp.charAt(i));
+            }
+        }
+        return newString;
     }
 
     public static int[] allIndexOf (String string, char chr) {
